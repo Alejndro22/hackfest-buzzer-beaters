b86c5c2fa0a3cb174f3791a9d10cfcd0130e1fb5
Yossu502 <jossugames@gmail.com>
2022-06-20 23:09:38 -0600

    agregando NavBar e index

---
+++ impulso/componentes/NavBar.js
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import BurguerButton from './BurguerButton'
+
+function NavBar() {
+  return (
+    <nav className='NavBarComponent'>
+      <div className='General_Container'>
+        <ul className='General_Container__ListPages'>
+          <li className='links'>
+            <Link href='' passHref>
+              <a>Explorar</a>
+            </Link>
+          </li>
+          <li className='links'>
+            <Link href='#comofunciona' passHref>
+              <a>Cómo Funciona</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='' passHref>
+              <Image src='/logo.png' alt='logo de la empresa' layout='fixed' width={200} height={200} className='imageLogo'></Image>
+            </Link>
+          </li>
+          <li className='links'>
+            <Link href='#ObjetivoODS' passHref>
+              <a>Objetivo 9 ODS</a>
+            </Link>
+          </li>
+          <li className='links'>
+            <Link href='' passHref>
+              <a>Iniciar Sesion</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className='burguer'>
+        <BurguerButton></BurguerButton>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
--- impulso/pages/_app.js
+++ impulso/pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '../styles/NavBar.css'
+import '../styles/BurguerButton.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
