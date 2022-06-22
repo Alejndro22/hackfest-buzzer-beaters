4acc39b2671fd3173f124b673995e477874d1670
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2022-06-22 17:51:57 -0600

    Se agregó tailwind al proyecto

--- impulso/pages/VerPublicaciones.js
+++ impulso/pages/VerPublicaciones.js
@@ -1,23 +1,26 @@
 import Publicaciones from "../components/Publicaciones";
 import AgregarPublicacion from "../components/AgregarPublicacion";
 
-
 function VerPublicaciones(props) {
   return (
-    <div className="principal">
-      <div className="tituloGeneeral"><h1><b>Publicaciones</b></h1></div>
-        <div className="contenedorGeneral">
-            <div className="contenedorPost">
-                <Publicaciones></Publicaciones>
-            </div>
-            <div className="contenedorAgregar">
-                <AgregarPublicacion></AgregarPublicacion>
-            </div>
+    <div className="principal ">
+      <div className="tituloGeneeral">
+        <h1>
+          <b>Publicaciones</b>
+        </h1>
+      </div>
+      <div className="contenedorGeneral">
+        <div className="contenedorPost">
+          <Publicaciones></Publicaciones>
         </div>
+        <div className="contenedorAgregar">
+          <AgregarPublicacion></AgregarPublicacion>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-VerPublicaciones.propTypes = {}
+VerPublicaciones.propTypes = {};
 
-export default VerPublicaciones
+export default VerPublicaciones;
