430b6d4c34916b72f60d0de6789f8f0d08bb8011
GeovannyTS <38725404+geovannysalvador@users.noreply.github.com>
2022-06-22 17:38:44 -0600

    Pagina ver
    Se corrigen errores que se presentaron en la pagina ver publicaciones

---
+++ impulso/pages/VerPublicaciones.js
@@ -0,0 +1,23 @@
+import Publicaciones from "../components/Publicaciones";
+import AgregarPublicacion from "../components/AgregarPublicacion";
+
+
+function VerPublicaciones(props) {
+  return (
+    <div className="principal">
+      <div className="tituloGeneeral"><h1><b>Publicaciones</b></h1></div>
+        <div className="contenedorGeneral">
+            <div className="contenedorPost">
+                <Publicaciones></Publicaciones>
+            </div>
+            <div className="contenedorAgregar">
+                <AgregarPublicacion></AgregarPublicacion>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+VerPublicaciones.propTypes = {}
+
+export default VerPublicaciones
