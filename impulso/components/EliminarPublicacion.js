d10ace70a3055d90141128ca822c5dcb964e0f5b
Alejndro22 <38710085+Alejndro22@users.noreply.github.com>
2022-06-25 01:32:48 -0600

    Cambios en página explorar

--- impulso/components/EliminarPublicacion.js
+++ impulso/components/EliminarPublicacion.js
@@ -1,23 +1,27 @@
-import { async } from '@firebase/util'
-import { deleteDoc, doc } from 'firebase/firestore'
-import { deleteObject, ref } from 'firebase/storage'
-import React from 'react'
-import { db, storage } from '../firebase/client'
+import { async } from "@firebase/util";
+import { deleteDoc, doc } from "firebase/firestore";
+import { deleteObject, ref } from "firebase/storage";
+import React from "react";
+import { db, storage } from "../firebase/client";
 
-export default function EliminarPublicacion({id}) {
-    
-    const procesoEliminar = async()=>{
-        try {
-            await deleteDoc(doc(db,"Publicaciones",id))
-            console.log("El articulo se ha eliminado exitosamentemente")
-        } catch (error) {
-            console.log("Ha ocurrido un error: " + error)
-        }
+export default function EliminarPublicacion({ id }) {
+  const procesoEliminar = async () => {
+    try {
+      await deleteDoc(doc(db, "Publicaciones", id));
+      console.log("El articulo se ha eliminado exitosamentemente");
+    } catch (error) {
+      console.log("Ha ocurrido un error: " + error);
     }
-    
-    return (
-        <div>
-            <button className = 'btn btn-danger' onClick={procesoEliminar} >Eliminar</button>
-        </div>
-    )
+  };
+
+  return (
+    <div>
+      <button
+        className="border border-amber-600 text-amber-600 hover:bg-rose-600 hover:text-white font-bold py-2 px-4 rounded"
+        onClick={procesoEliminar}
+      >
+        Eliminar
+      </button>
+    </div>
+  );
 }
--- impulso/components/Publicaciones.js
+++ impulso/components/Publicaciones.js
@@ -1,6 +1,12 @@
 import { React, useState, useEffect } from "react";
 import { db } from "../firebase/client.js";
 import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  BiCalendar,
+  BiMessageAltDetail,
+  BiLinkExternal,
+  BiLocationPlus,
+} from "react-icons/bi";
 import EliminarPublicacion from "../components/EliminarPublicacion";
 import Image from "next/image";
 
@@ -20,36 +26,57 @@ export default function Publicaciones() {
     });
   }, []);
   return (
-    <div className="grid grid-cols-2 gap-4 mx-4">
+    <div className="grid md:grid-cols-2 sm:grid-cols-1 gap-4 mx-4">
       {publicaciones.length === 0 ? (
         <p>No hay publicaciones</p>
       ) : (
         publicaciones.map((publicacion) => (
           <div
-            className="bg-white break-inside-avoid rounded-lg mb-4 text-left justify-between h-64 w-auto border border-amber-600 shadow-md col-span-1 "
+            className="bg-white break-inside-avoid rounded-lg mb-4 text-left justify-between h-52 w-auto border border-amber-600 shadow-md col-span-1 "
             key={publicacion.id}
           >
-            <div className="grid grid-cols-4 gap-x-4 p-2">
-              <div className="col-span-1">
+            <div className="grid grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-x-4 p-2">
+              <div className="col-span-1 w-full relative">
                 <Image
                   src={publicacion.imagen}
                   alt="titulo"
-                  width="200px"
-                  height="200px"
-                  className="rounded-lg"
+                  width="100%"
+                  height="100%"
+                  layout="fill"
+                  className="rounded-lg "
                 ></Image>
               </div>
-              <div className="col-span-3">
-                <h2 className="text-amber-600 text-2xl">
-                  {publicacion.titulo}
-                </h2>
-                <p className="text-slate-600">
-                  {publicacion.createdAt.toDate().toDateString()}
-                </p>
-                <p className="truncate">{publicacion.descripcion}</p>
-                <p>{publicacion.enlace}</p>
-                <p>{publicacion.ubicacion}</p>
-                <EliminarPublicacion id={publicacion.id} />
+              <div className="col-span-3 lg:col-span-4 xl:col-span-5">
+                <div className="flex place-items-center justify-between">
+                  <h2 className="text-amber-600 text-2xl truncate">
+                    {publicacion.titulo}
+                  </h2>
+                  <EliminarPublicacion className="mr-4 " id={publicacion.id} />
+                </div>
+                <div className="place-items-center flex">
+                  <BiCalendar className="mr-2 shrink-0"></BiCalendar>
+                  <p className="text-slate-600 truncate">
+                    {publicacion.createdAt.toDate().toDateString()}
+                  </p>
+                </div>
+                <div className="place-items-center flex">
+                  <BiLinkExternal className="mr-2 shrink-0"></BiLinkExternal>
+                  <a
+                    href={publicacion.enlace}
+                    className="truncate"
+                    target="_blank"
+                  >
+                    {publicacion.enlace}
+                  </a>
+                </div>
+                <div className="place-items-center flex">
+                  <BiLocationPlus className="mr-2 shrink-0"></BiLocationPlus>
+                  <p className="truncate">{publicacion.ubicacion}</p>
+                </div>
+                <div className="flex">
+                  <BiMessageAltDetail className="mr-2 shrink-0 mt-2"></BiMessageAltDetail>
+                  <p className="truncate">{publicacion.descripcion}</p>
+                </div>
               </div>
             </div>
           </div>
